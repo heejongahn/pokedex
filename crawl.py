b2d581d4736d1bba384e42ab86a2730a2385df6a
Heejong Ahn <heejongahn@gmail.com>
2016-10-24 17:32:28 +0900

    Add crawl_pokemon function

--- crawl.py
+++ crawl.py
@@ -1,9 +1,14 @@
 from requests import get
 from lxml import html
 
-BASE_KR = 'http://ko.pokemon.wikia.com/wiki/{}'
-BASE_EN = 'http://www.pokemon.com/us/pokedex/{}'
-NAME_MAP = 'http://ko.pokemon.wikia.com/wiki/%EA%B5%AD%EA%B0%80%EB%B3%84_%ED%8F%AC%EC%BC%93%EB%AA%AC_%EC%9D%B4%EB%A6%84_%EB%AA%A9%EB%A1%9D'
+from .models import LocaleType
+
+BASE_URLS = {
+        LocaleType.KR: 'http://ko.pokemon.wikia.com/wiki/{}'
+        LocaleType.EN: 'http://www.pokemon.com/us/pokedex/{}'
+        }
+
+NAME_MAP_URL = 'http://ko.pokemon.wikia.com/wiki/%EA%B5%AD%EA%B0%80%EB%B3%84_%ED%8F%AC%EC%BC%93%EB%AA%AC_%EC%9D%B4%EB%A6%84_%EB%AA%A9%EB%A1%9D'
 
 def fetch_and_parse(url):
     r = get(url)
@@ -13,7 +18,7 @@ def fetch_and_parse(url):
 
 def construct_name_map():
     name_map = {}
-    doc = fetch_and_parse(NAME_MAP)
+    doc = fetch_and_parse(NAME_MAP_URL)
 
     table = doc.cssselect('div.WikiaArticle table.prettytable')[0]
     rows = table.findall('tr')[1:]
@@ -30,3 +35,11 @@ def construct_name_map():
         name_map[poke_no] = (kr_name, jp_name, en_name)
 
     return name_map
+
+def crawl_pokemon(locale, name):
+    doc = fetch_and_parse(BASE_URLS[locale].format(name))
+
+    if locale == LocaleType.KR:
+        crawl_pokemon_kr(doc)
+    else:
+        crawl_pokemon_en(doc)
