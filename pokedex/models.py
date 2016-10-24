347c0f70cb339d1f9861b071199756224ef32eeb
Heejong Ahn <heejongahn@gmail.com>
2016-10-24 21:03:04 +0900

    Add relation and constructor to models

--- pokedex/models.py
+++ pokedex/models.py
@@ -25,6 +25,15 @@ class Pokemon(db.Model):
     poke_type = db.Column(db.String(20))
     height = db.Column(db.Float)
     weight = db.Column(db.Float)
+    locales = db.relationship('PokemonLocale', backref='basics')
+
+    def __init__(self, p_id, i, g, p_type, h, w):
+        self.poke_id = p_id
+        self.image_url = i
+        self.gender = g
+        self.poke_type = p_type
+        self.height = h
+        self.weight = w
 
 
 class PokemonLocale(db.Model):
@@ -33,3 +42,11 @@ class PokemonLocale(db.Model):
     name = db.Column(db.String(20))
     description = db.Column(db.Text)
     category = db.Column(db.String(20))
+    basics_id = db.Column(db.Integer, db.ForeignKey('pokemon.poke_id'))
+
+    def __init__(self, p_id, l, n, d, c):
+        self.poke_id = p_id
+        self.locale = l
+        self.name = n
+        self.description = d
+        self.category = c
