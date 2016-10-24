4f56b746fa57ff17c7d2bdb5ec24faf10d5eb3c9
Heejong Ahn <heejongahn@gmail.com>
2016-10-24 12:49:50 +0900

    Add name map construction logic into app init sequence

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import crawl
+
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+name_map = None
 
 @app.route('/')
 def index():
     return "Warmest welcome from pokedex."
 
 if __name__ == '__main__':
+    name_map = crawl.construct_name_map()
     app.run()
