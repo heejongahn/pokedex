6711a6caefed30fff67e1fce571a708690c15f98
Heejong Ahn <heejongahn@gmail.com>
2016-10-24 23:46:04 +0900

    Add search bar inside of nav tag

--- pokedex/assets/main.js
+++ pokedex/assets/main.js
@@ -1,1 +1,9 @@
 require('./pokedex');
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  window.location.href = `/${searchInput.value}`;
+});
