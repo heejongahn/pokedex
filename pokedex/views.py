c614d5ba266d2117621e90debc70e57511c31389
Heejong Ahn <heejongahn@gmail.com>
2016-10-24 22:36:44 +0900

    Add check-and-create logic to pokemon endpoint

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, flash, session
 
-from pokedex import app
+from pokedex import app, db
 from pokedex.models import LocaleType, GenderType, Pokemon, PokemonLocale
 from pokedex.crawl import construct_name_map, crawl_pokemon
 
@@ -26,4 +26,21 @@ def init_view(app):
         if name not in name_array_map[LocaleType.EN]:
             return "No such pokemon :("
 
-        return str(crawl_pokemon(LocaleType.EN, name))
+        info = crawl_pokemon(LocaleType.EN, name)
+        (poke_id, image_url, gender, poke_type, height, weight) = info[0]
+        (name, description, category) = info[1]
+
+        p = Pokemon.query.get(poke_id)
+        if p is None:
+            p = Pokemon(poke_id, image_url, gender, poke_type, height, weight)
+            db.session.add(p)
+            db.session.commit()
+
+        p_locale = PokemonLocale.query.get((poke_id, LocaleType.EN))
+        if p_locale is None:
+            p_locale = PokemonLocale(poke_id, LocaleType.EN, name, description,
+                    category, p.poke_id)
+            db.session.add(p_locale)
+            db.session.commit()
+
+        return render_template('pokemon.html', p=p, p_locale=p_locale)
