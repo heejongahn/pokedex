344c94ee13783c2a7444d483eb0016a2b16f6696
Heejong Ahn <heejongahn@gmail.com>
2016-10-24 17:18:25 +0900

    Add (revised) model and schema

---
+++ models.py
@@ -0,0 +1,33 @@
+import enum
+from server import db
+
+# Enum Definitions
+
+class LocaleType(enum.Enum):
+    KR = "kr"
+    JP = "jp"
+    EN = "en"
+
+
+class GenderType(enum.Enum):
+    ASEXUAL = "X"
+    ONLY_MALE = "♂"
+    ONLY_FEMALE = "♀"
+    BOTH_GENDER = "♀ ♂"
+
+
+# Actual Models
+
+class Pokemon(db.Model):
+    poke_id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(120))
+    gender = db.Column(db.Enum(GenderType))
+    poke_type = db.Column(db.String(20))
+
+
+class PokemonLocale(db.Model):
+    poke_id = db.Column(db.Integer, primary_key=True)
+    locale = db.Column(db.Enum(LocaleType), primary_key=True)
+    name = db.Column(db.String(20))
+    description = db.Column(db.Text))
+    category = db.Column(db.String(20))
