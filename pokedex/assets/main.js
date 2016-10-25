01979e131fb9f159a543be9ace954af5fc7d9443
Heejong Ahn <heejongahn@gmail.com>
2016-10-25 16:50:17 +0900

    Add spinner, when user submits search form

--- pokedex/assets/main.js
+++ pokedex/assets/main.js
@@ -5,6 +5,7 @@ const searchInput = document.getElementById('search-input');
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  showSpinner();
   window.location.href = `/${searchInput.value}`;
 });
 
@@ -17,4 +18,11 @@ backToTopBtn.addEventListener('click', (e) => {
 const hasVScroll = document.body.scrollHeight > window.innerHeight;
 if (!hasVScroll) {
   backToTopBtn.style.display = 'none';
+}
+
+const showSpinner = () => {
+  const spinner = document.getElementsByClassName('spinner')[0]
+  if (spinner) {
+    spinner.style.display = 'block';
+  }
 }
